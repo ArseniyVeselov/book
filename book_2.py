aa7264f7b6924253d8c34fdd1ec1f395a708caa0
ArseniyVeselov <120600466+ArseniyVeselov@users.noreply.github.com>
2022-12-17 20:57:29 +0300

    Update book_2.py

--- book_2.py
+++ book_2.py
@@ -1,39 +1,37 @@
-name_l , phone_l= ['Пупкин Залупкин Олег'], ['89032787787'] # общие списки
+def adding():
+    def fio_list(name):                # добавление в список ФИО
+         name_l.append(name)
 
-def fio_list(name):                # добавление в список ФИО
-     name_l.append(name)
+    def phone_list(phone):             # добавление в список номеров
+         n=[i for i in phone if i in '0123456789']
+         phone_l.append(''.join(n))
 
-def phone_list(phone):             # добавление в список номеров
-     n1=[i for i in phone if i in '0123456789']
-     phone_l.append(''.join(n1))
+def removal():               # удаление записи
+    #
+    number = input('Введи номер записи для удаления')
+    if number == '1' or number == '2':
+        del name_l[int(number) - 1]
+        del phone_l[int(number) - 1]
+    #    
 
-def records():                      # отображение списка
-     counter = 0
-     for i in range(len(name_l)):
-          counter += 1
-          print(counter, 'ФИО:', name_l[i], 'Телефон:', phone_l[i])
 
-def removal(number):               # удаление записи
-     if number == '1' or number == '2':
-          del name_l[int(number) - 1]
-          del phone_l[int(number) - 1]
-          records()
-def amount():                      # количество записей
-     print('Количество записей =',len(name_l))
+def greetings():
+    print('Выбери действие:','Добавление записи -> 1','Удаление записи -> 2', 'Показать список -> 3', 'Закончить работу программы -> 4',sep='\n')
+    action = input()
+    if action == '1':
+        adding()
+    elif action == '2':
+        removal
 
-amount()
-records()
+name_l, phone_l = [],[]
+        
+        
+greetings()
+adding()
+    
 
-name = input('Для добвавления введи ФИО: ').title().strip()
+name = input('Введи ФИО: ').title().strip()
 phone = input('Введи номер: ').strip()
-
-fio_list(name)
-phone_list(phone)
-records()
-
-number = input('Удалит запись 1 или 2 ? ')
-removal(number)
-amount()
 
 
 
