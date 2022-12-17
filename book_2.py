5c8fe3ca236fc5c30048f7b788dc1515b88440cc
ArseniyVeselov <120600466+ArseniyVeselov@users.noreply.github.com>
2022-12-17 18:52:51 +0300

    Update book_2.py

--- book_2.py
+++ book_2.py
@@ -7,34 +7,33 @@ def phone_list(phone):             # добавление в список ном
      n1=[i for i in phone if i in '0123456789']
      phone_l.append(''.join(n1))
 
-def zapisi():                      # отображение списка
+def records():                      # отображение списка
      counter = 0
      for i in range(len(name_l)):
           counter += 1
           print(counter, 'ФИО:', name_l[i], 'Телефон:', phone_l[i])
 
-def udalenie(nomer):               # удаление записи
-     if nomer == '1' or nomer == '2':
-          del name_l[int(nomer) - 1]
-          del phone_l[int(nomer) - 1]
-          zapisi()
-def kolichestvo():                 # количество записей
+def removal(number):               # удаление записи
+     if number == '1' or number == '2':
+          del name_l[int(number) - 1]
+          del phone_l[int(number) - 1]
+          records()
+def amount():                      # количество записей
      print('Количество записей =',len(name_l))
 
-kolichestvo()
-zapisi()
+amount()
+records()
 
 name = input('Для добвавления введи ФИО: ').title().strip()
 phone = input('Введи номер: ').strip()
 
 fio_list(name)
 phone_list(phone)
-zapisi()
+records()
 
-nomer = input('Удалит запись 1 или 2 ? ')
-udalenie(nomer)
-kolichestvo()
-
+number = input('Удалит запись 1 или 2 ? ')
+removal(number)
+amount()
 
 
 
